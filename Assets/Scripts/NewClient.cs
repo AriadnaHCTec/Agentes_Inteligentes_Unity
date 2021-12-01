d1ca3b1255f0e7c21499d8602e7d2ec1cc34b3f5
AriadnaHCTec <65170456+AriadnaHCTec@users.noreply.github.com>
2021-12-01 12:48:35 -0600

    Agrupado

--- Assets/Scripts/NewClient.cs
+++ Assets/Scripts/NewClient.cs
@@ -9,14 +9,14 @@ public class NewClient : MonoBehaviour
     List<List<Vector3>> positions;
 
     public GameObject[] spheres;
+    public GameObject[] lights;
     public float timeToUpdate = 5.0f;
     private float timer;
     public float dt;
 
     // IEnumerator - yield return
-    IEnumerator SendData(string data)
+    IEnumerator SendData()
     {
-        //http://agentesinteligentes-e4.us-south.cf.appdomain.cloud/updatePositions
         UnityWebRequest www = UnityWebRequest.Get("http://agentesinteligentes-e4.us-south.cf.appdomain.cloud/updatePositions");
         yield return www.Send();
 
@@ -27,7 +27,7 @@ public class NewClient : MonoBehaviour
         else
         {
             // Show results as text
-            Debug.Log(www.downloadHandler.text);
+            //Debug.Log(www.downloadHandler.text);
 
             // Or retrieve results as binary data
             List<Vector3> newPositions = new List<Vector3>();
@@ -38,7 +38,7 @@ public class NewClient : MonoBehaviour
             if(spheres.Length > 1)
                 txt = txt + '}';
             string[] strs = txt.Split(new string[] { "}, {" }, StringSplitOptions.None);
-            Debug.Log("strs.Length:" + strs.Length);
+            //Debug.Log("strs.Length:" + strs.Length);
             for (int i = 0; i < strs.Length; i++)
             {
                 strs[i] = strs[i].Trim();
@@ -60,17 +60,59 @@ public class NewClient : MonoBehaviour
       
     }
 
+
+
+    IEnumerator SendLights()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://agentesinteligentes-e4.us-south.cf.appdomain.cloud/trafficLight");
+        yield return www.Send();
+
+        if (www.isError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            //Debug.Log(www.downloadHandler.text);
+
+            string txt = www.downloadHandler.text.Replace('\'', '\"');
+            Debug.Log(txt[11] + " " + txt[25] + " " + txt[39] + " " + txt[53]);
+            changeColor(0, txt[11] - '0');
+            changeColor(1, txt[25] - '0');
+            /*changeColor(2, txt[39] - '0');
+            changeColor(3, txt[53] - '0');*/
+        }
+    }
+
+    void changeColor(int index, int state)
+    {
+        GameObject red= lights[index].transform.GetChild(0).GetChild(0).gameObject;
+        GameObject yellow= lights[index].transform.GetChild(0).GetChild(1).gameObject;
+        GameObject green = lights[index].transform.GetChild(0).GetChild(2).gameObject;
+        red.SetActive(false);
+        yellow.SetActive(false);
+        green.SetActive(false);
+        if (state == 0)
+        {
+            red.SetActive(true);
+        }
+        else if(state == 1)
+        {
+            yellow.SetActive(true);
+        }
+        else
+        {
+            green.SetActive(true);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         positions = new List<List<Vector3>>();
-        Debug.Log(spheres.Length);
 #if UNITY_EDITOR
-        //string call = "WAAAAASSSSSAAAAAAAAAAP?";
-        Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);
-        string json = EditorJsonUtility.ToJson(fakePos);
-        //StartCoroutine(SendData(call));
-        StartCoroutine(SendData(json));
+        //StartCoroutine(SendData());
+        StartCoroutine(SendLights());
         timer = timeToUpdate;
 #endif
     }
@@ -89,14 +131,13 @@ public class NewClient : MonoBehaviour
         {
 #if UNITY_EDITOR
             timer = timeToUpdate; // reset the timer
-            Vector3 fakePos = new Vector3(3.44f, 0, -15.707f);
-            string json = EditorJsonUtility.ToJson(fakePos);
-            StartCoroutine(SendData(json));
+            //StartCoroutine(SendData());
+            StartCoroutine(SendLights());
 #endif
         }
 
 
-        if (positions.Count > 1)
+        /*if (positions.Count > 1)
         {
             for (int s = 0; s < spheres.Length; s++)
             {
@@ -111,6 +152,6 @@ public class NewClient : MonoBehaviour
                 Vector3 dir = last[s] - prevLast[s];
                 spheres[s].transform.rotation = Quaternion.LookRotation(dir);
             }
-        }
+        }*/
     }
 }
