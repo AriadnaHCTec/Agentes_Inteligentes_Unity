66f455b7c3b0715043f2ca2761cd67c3dcd5e54d
Rbn-Sanchez <A01378379@itesm.mx>
2021-12-01 21:18:11 -0600

    Movimiento en las llantas de los coches

--- Assets/Scripts/RotateWheels.cs
+++ Assets/Scripts/RotateWheels.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class RotateWheels : MonoBehaviour
 {
-    public bool rotateWheel;
+    public GameObject parent;
+    private Transform transf;
+    private Vector3 lastPos;
+
     private Vector3 rotVec = new Vector3(0,-1,0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        transf = parent.transform;
+        lastPos = transf.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rotateWheel)
+        if(lastPos != transf.position)
             transform.Rotate(rotVec,Space.Self);
+        lastPos = transf.position;
     }
 }
